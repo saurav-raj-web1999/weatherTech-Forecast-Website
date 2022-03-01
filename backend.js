685f3019e8c8647b2529f5c78e89624a7315cb74
Saurav <sauravrajjha1999@gmail.com>
2022-03-01 21:40:35 +0530

    Added handlebar files

--- backend.js
+++ backend.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+// const hbs = require('hbs');
 const app = express();
 const port = process.env.PORT || 8000;
 
 const errorPage = fs.readFileSync('errorPage.html','utf-8');
 let staticPath = __dirname;
 
+app.set('view engine','hbs');
+
 app.use(express.static(staticPath));
 
-app.get("",(req,res)=>{
-    res.send();
-});
+app.get("", (req,res)=>{
+    res.render('index');
+})
+
+app.get("/weather",(req,res)=>{
+    res.render('weather')
+})
 
 app.get("*",(req, res)=>{
-    res.send(errorPage);
+    res.render('errorPage');
 })
 
 app.listen(port, ()=>{
